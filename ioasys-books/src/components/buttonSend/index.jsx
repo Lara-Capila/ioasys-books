c0e5cb6d614f87da240af2fb8903a932e8be0c2d
Lara Capila <laracapilak@gmail.com>
2021-12-13 11:07:33 -0300

    feat(Button): Cria compoenten botão de enviar formulário

---
+++ ioasys-books/src/components/buttonSend/index.jsx
@@ -0,0 +1,10 @@
+import { Button } from 'antd';
+import React from 'react';
+
+export default function ButtonSend() {
+  return (
+    <Button type="primary" htmlType="submit" className="login-form-button">
+      Entrar
+    </Button>
+  );
+}
