6cc8b143b0a6207da0aba33dc0f2c1d765d599b6
Lara-Capila <laracapilak@gmail.com>
2021-12-13 20:09:36 -0300

    feat(token): Adiciona token no localStorage

--- ioasys-books/src/services/api.js
+++ ioasys-books/src/services/api.js
@@ -4,4 +4,29 @@ const api = axios.create({
   baseURL: 'https://books.ioasys.com.br/api/v1/',
 });
 
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('access_token');
+    if (token && !config.noBearer) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+api.interceptors.response.use(
+  (response) => {
+    console.log(response, 'responseeeeeee');
+    if (response.headers?.authorization) {
+      localStorage.setItem('access_token', response.headers.authorization);
+    }
+    return response;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
 export default api;
