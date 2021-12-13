75779f3e53eb9cf34d7c841bcafdc1218605e736
Lara Capila <laracapilak@gmail.com>
2021-12-13 11:06:09 -0300

    refactor(Login): Refatora componente Login para utilização da biblioteca antd

--- ioasys-books/src/components/Form/LoginForm.jsx
+++ ioasys-books/src/components/Form/LoginForm.jsx
@@ -1,26 +1,56 @@
 import React from 'react';
 
-import { Formik, Form, Field, ErrorMessage } from 'formik';
-import LoginSchema from './validationSchema';
+import { Form } from 'antd';
+
 import * as Styled from './styles';
+import ButtonSend from '../buttonSend';
 
 export default function LoginForm() {
+  const onFinish = (values) => {
+    console.log('Received values of form: ', values);
+  };
+
   return (
     <Styled.Container>
-      <Formik
-        initialValues={{ email: '', senha: '' }}
-        validationSchema={LoginSchema}
+      <Form
+        name="normal_login"
+        className="login-form"
+        initialValues={{}}
+        onFinish={onFinish}
       >
-        <Form>
-          <label htmlFor="email">Email</label>
-          <Field id="email" name="email" placeholder="Email" />
-          <ErrorMessage name="email" />
+        <Form.Item
+          name="Email"
+          rules={[
+            {
+              required: true,
+              message: 'Por favor, insira seu email.',
+            },
+            { type: 'email', message: 'Insira um formato válido.' },
+            { whitespace: false },
+          ]}
+          hasFeedback
+        >
+          <Styled.InputField placeholder="Email" />
+        </Form.Item>
+        <Form.Item
+          name="senha"
+          rules={[
+            {
+              required: true,
+              message: 'Por favor, insira sua senha.',
+            },
+            { min: 6 },
+            { max: 12 },
+          ]}
+          hasFeedback
+        >
+          <Styled.InputField type="password" placeholder="Senha" />
+        </Form.Item>
 
-          <label htmlFor="senha">Senha</label>
-          <Field name="senha" type="password" placeholder="Email" />
-          <ErrorMessage name="email" />
-        </Form>
-      </Formik>
+        <Form.Item>
+          <ButtonSend />
+        </Form.Item>
+      </Form>
     </Styled.Container>
   );
 }
