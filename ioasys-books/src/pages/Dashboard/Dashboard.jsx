b04e3724dc78be2bff16c8d310b92c9e5c28a0e5
Lara-Capila <laracapilak@gmail.com>
2021-12-14 16:31:18 -0300

    feat(): Adiciona sessão de logout e de usuário do dashboard

--- ioasys-books/src/pages/Dashboard/Dashboard.jsx
+++ ioasys-books/src/pages/Dashboard/Dashboard.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 
+import { useHistory } from 'react-router-dom';
 import * as Styled from './styles';
 import Title from '../../components/Title/Title';
 import BookCard from '../../components/BookCard/BookCard';
 import getToken from '../../helpers/getToken';
 import getBooks from '../../services/Books/getBooks';
+import getUser from '../../helpers/getUser';
 
 function DashboardPage() {
   const [bookList, setBookList] = React.useState();
+  const [userData, setUserData] = React.useState();
+
+  const history = useHistory();
 
   const fetchBook = async () => {
     const token = getToken();
@@ -18,22 +23,43 @@ function DashboardPage() {
         category: 'biographies',
       });
       setBookList(responseBooks.data.data);
-      console.log(responseBooks.data);
     } catch (err) {
-      console.log(err);
+      throw new Error(err);
+    }
+  };
+
+  const getUserData = async () => {
+    const user = await getUser();
+    setUserData(user);
+  };
+
+  const logoutUser = () => {
+    try {
+      localStorage.clear();
+      history.push('/');
+    } catch (err) {
+      throw new Error(err);
     }
   };
 
   React.useEffect(() => {
     fetchBook();
+    getUserData();
   }, []);
 
+  console.log(userData);
   return (
     <Styled.Container>
       <Styled.Header>
         <Styled.LogoContainer>
           <Title />
         </Styled.LogoContainer>
+        <Styled.ContainerUser>
+          <Styled.User>
+            Bem Vindo, <strong>{userData?.name}!</strong>
+          </Styled.User>
+          <Styled.LogoutIcon onClick={logoutUser} />
+        </Styled.ContainerUser>
       </Styled.Header>
       <Styled.BooksCard>
         {bookList?.map((book) => (
